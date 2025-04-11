9531196e04f9576d327a435f65e0c58e9fa084e3
alrigo123 <alexestrada2804@gmail.com>
2025-04-11 08:05:19 -0500

    Footer Change

--- client/src/LandingPage/FooterComp.js
+++ client/src/LandingPage/FooterComp.js
@@ -5,8 +5,10 @@ const FooterComp = () => {
     return (
         <div className='mb-0'>
             <footer className="footer">
-                <p>Desarrollado por <strong>OPPM / Oficina de Informática</strong></p>
-                <p className='fw-bold'>&copy; {new Date().getFullYear()} GERAGRI CUSCO</p>
+                <div className="text-center small">
+                    <p className="mb-0">© GERAGRI {new Date().getFullYear()} Gerencia Regional de Agricultura - Cusco. Todos los derechos reservados.</p>
+                    <p>Diseñado por <strong>OPPM / Oficina de Informática</strong></p>
+                </div>
             </footer>
         </div>
     )
